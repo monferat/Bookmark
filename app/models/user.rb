8adf098beaef36778d487a88d0e8d3c3863735c4
Bezpalchuk V <bezpalchukv@gmail.com>
2017-10-25 01:36:14 +0300

    Add Facebook authentication

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :omniauthable, omniauth_providers: [:facebook]
+
+  def self.create_from_omniauth(params)
+    user = find_or_create_by(email: params.info.email)
+    user.update(token: params.credentials.token,
+                name: params.info.name,
+                email: params.info.email,
+                avatar: params.info.image)
+    user
+  end
+
+  def fbgraph
+    raise Exceptions::Facebook::NotPresent unless token
+    Facebook.fbgraph(token)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+
+  devise_for :users, controllers: { omniauth_callbacks: 'auth/callbacks' }
+
+  devise_scope :user do
+    delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
 end
