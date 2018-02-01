4896aad4f85efb1fee50fe2b06a58413881dd42e
Bezpalchuk V <bezpalchukv@gmail.com>
2018-02-02 01:01:01 +0200

    Add paperclip and aws config

--- app/models/bookmarker.rb
+++ app/models/bookmarker.rb
@@ -1,10 +1,12 @@
 class Bookmarker < ApplicationRecord
   include PgSearch
 
-  has_attached_file :snapshot, styles: { medium: '300x300#' }, default_url: '/images/missing.png'
-  # has_attached_file :snapshot, :storage => :s3,
-  # :s3_credentials => "#{Rails.root}/config/s3.yml"
-  validates_attachment_content_type :snapshot, content_type: /\Aimage\/.*\z/
+  # has_attached_file :snapshot, styles: { medium: '300x300#' }, default_url: '/images/missing.png'
+  # has_attached_file :snapshot, styles: { medium: '300x300#' }, :storage => :s3, :s3_credentials => "#{Rails.root}/config/aws.yml"
+  # validates_attachment_content_type :snapshot, content_type: /\Aimage\/.*\z/
+
+  has_attached_file :snapshot, styles: { medium: '300x300#', thumb: '200x200#' }, default_url: '/images/missing.png'
+  validates_attachment :snapshot, content_type: { content_type: ['image/jpg', 'image/jpeg', 'image/png'] }
 
   belongs_to :user
 
